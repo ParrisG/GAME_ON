1afe1ae6beda65d1793fab317cb2152b07d05c6b
anju-kumaran <anju.naduth@gmail.com>
2021-11-23 14:01:29 -0500

    user authentication and logout using jwt

--- backend/app.js
+++ backend/app.js
@@ -13,31 +13,12 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors({
-  origin: ['http://localhost:3000/'],
-  methods: ['GET', 'POST'],
-  credentials: true
-}));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  session({
-    key: "userId",
-    secret: "secret",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      expires: 60 * 60 * 24
-    }
-  })
-)
+app.use(cors());
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,39 +1,33 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import FilterBox from "./FilterBox";
 import StockList from "./StockList";
 import axios from "axios";
-import Button from "@restart/ui/esm/Button";
+import jwt from 'jsonwebtoken';
+import { Button } from 'react-bootstrap';
 
 export default function Dashboard(props) {
-  axios.defaults.withCredentials = true;
-  const [loginStatus, setLoginStatus] = useState('');
+  
+  const navigate = useNavigate();
 
   useEffect(() => {
-    axios.get('/users/authenticate').then((response) => {
-      console.log('line 13',response.data);
+		const token = localStorage.getItem('token');
     
-      if(response.data.loggedIn === true){
-        //console.log('line num 16:',response.data.loggedIn);
-        setLoginStatus(response.data.user[0].email);
-      }
-    });
-  }, []);
+    if(token){
+      const user = jwt.decode(token);
+      console.log('This is Success', user);
+    } else {
+      console.log('Line NUmber 34');
+      localStorage.removeItem('token');
+      alert('You have to Login To view this page');
+      navigate('/login');
+    }
+	}, [])
+
 
   function logout() {
-    localStorage.clear();
-    // Remove saved data from sessionStorage
-    sessionStorage.removeItem('userId');
-    return axios.post('/users/logout',{
-      //email: emailRef.current.value,
-      //password: passwordRef.current.value
-    }).then((response) => {
-      console.log(response);
-      window.location.href = '/login';
-      //navigate('/dashboard');
-      //history.push('/dashboard');
-    });
-
-    window.location.href = '/login';
+    localStorage.removeItem('token');
+    navigate('/login');    
   }
 
   return (
