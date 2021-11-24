5f4edb9c817299042786fbdd31fe7549d9925cac
Parris Greaves <parris.greaves@gmail.com>
2021-11-23 22:43:32 -0500

    add axios call to single stock summary endpoint.

--- frontend/src/components/StockDetail.jsx
+++ frontend/src/components/StockDetail.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
+import axios from "axios";
 
 
 import StockInformation from "./StockInformation";
@@ -8,8 +9,30 @@ import StockNewsList from "./StockNewsList";
 
 export default function StockDetail(props) {
 
+  const [stockInfo, setStockInfo] = useState();
+
   let params = useParams(); //we can access the :ticker value as params.ticker
 
+  const singleStockOptions = () => {
+    return {
+      method: 'GET',
+      url: 'https://yh-finance.p.rapidapi.com/stock/v2/get-summary',
+      params: {symbol: params.ticker},
+      headers: {
+        'x-rapidapi-host': process.env.REACT_APP_X_RAPIDAPI_HOST,
+        'x-rapidapi-key': process.env.REACT_APP_X_RAPIDAPI_KEY
+      }
+    }
+  };
+
+  useEffect(() => {
+    axios.request(singleStockOptions()).then(function (response) {
+      console.log(response.data);
+      setStockInfo(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    })
+  }, []);
 
   return (
     <div>
