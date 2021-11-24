4b1c33853c41c679f5a6b87958975e5531ea8502
Parris Greaves <parris.greaves@gmail.com>
2021-11-23 21:02:01 -0500

    add parsedStocks to StockList and pass props to StockListItem

--- frontend/src/components/StockList.jsx
+++ frontend/src/components/StockList.jsx
@@ -5,14 +5,17 @@ import { stockArrContext } from '../providers/StockArrProvider';
 
 export default function StockList(props) {
   const { stockArr } = useContext(stockArrContext);
-  console.log(stockArr);
+  
+  const parsedStocks = (stockArr).map((stock) => (
+    <StockListItem key={stock.symbol} name={stock.shortName} price={stock.regularMarketPrice.raw} percentChange={stock.regularMarketChangePercent.fmt} volume={stock.regularMarketVolume} />
+  ));
 
   return (
     <div>
       <h1>I am StockList</h1>
       <div>
         <h2>Here is StockListItem:</h2>
-        <StockListItem />
+        {parsedStocks}
       </div>
     </div>
   )
