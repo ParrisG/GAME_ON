cb0372305b61916e662b4de5270c1a5ec29fccbf
Parris Greaves <parris.greaves@gmail.com>
2021-11-26 07:52:56 -0500

    add more info to the StockInformation component

--- frontend/src/components/StockInformation.jsx
+++ frontend/src/components/StockInformation.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
-import {Card, Spinner, ListGroup} from 'react-bootstrap';
+import {Badge, Card, Spinner, ListGroup} from 'react-bootstrap';
 import axios from "axios";
 
 export default function StockInformation(props) {
@@ -43,17 +43,63 @@ export default function StockInformation(props) {
     } else {
       
         return (
-          <Card style={{ width: '30rem' }}>
+          <Card style={{ width: '30rem', background: 'rgb(99 99 99)'  }}>
             <Card.Body>
-              <Card.Title>{params.ticker}</Card.Title>
+              <Card.Title><h1>{params.ticker}</h1></Card.Title>
               <Card.Subtitle className="mb-2 text-muted">{stockInfo.price.shortName}</Card.Subtitle>
               <Card.Text>Exchange: {stockInfo.price.exchangeName}</Card.Text>
               <ListGroup>
+                <h1><Badge pill bg="dark">Price: {stockInfo.price.regularMarketPrice.fmt}</Badge></h1>
                 <ListGroup horizontal>
-                  <ListGroup.Item>Average Volume {stockInfo.price.averageDailyVolume10Day.longFmt}</ListGroup.Item>
-                  <ListGroup.Item>Day Volume {stockInfo.price.regularMarketVolume.longFmt}</ListGroup.Item>
+                  <Card style={{ width: '15rem' }}>
+                    <Card.Header>Volume</Card.Header>
+                    <Card.Body>{stockInfo.price.regularMarketVolume.longFmt}</Card.Body>
+                  </Card>
+                  <Card style={{ width: '15rem' }}>
+                    <Card.Header>Average Volume</Card.Header>
+                    <Card.Body>{stockInfo.price.averageDailyVolume10Day.longFmt}</Card.Body>
+                  </Card>
                 </ListGroup>
-                <ListGroup.Item>Summary {stockInfo.summaryProfile.longBusinessSummary}</ListGroup.Item>
+                <ListGroup horizontal>
+                  <Card style={{ width: '15rem' }}>
+                    <Card.Header>Open</Card.Header>
+                    <Card.Body>{stockInfo.price.regularMarketOpen.fmt}</Card.Body>
+                  </Card>
+                  <Card style={{ width: '15rem' }}>
+                    <Card.Header>Previous Close</Card.Header>
+                    <Card.Body>{stockInfo.price.regularMarketPreviousClose.fmt}</Card.Body>
+                  </Card>
+                </ListGroup>
+                <ListGroup horizontal>
+                  <Card style={{ width: '10rem' }}>
+                    <Card.Header>Change</Card.Header>
+                    <Card.Body>{stockInfo.price.regularMarketChange.fmt}</Card.Body>
+                  </Card>
+                  <Card style={{ width: '10rem' }}>
+                    <Card.Header>Change %</Card.Header>
+                    <Card.Body>{stockInfo.price.regularMarketChangePercent.fmt}</Card.Body>
+                  </Card>
+                  <Card style={{ width: '10rem' }}>
+                    <Card.Header>Beta</Card.Header>
+                    <Card.Body>{stockInfo.defaultKeyStatistics.beta.fmt}</Card.Body>
+                  </Card>
+                </ListGroup>
+                <ListGroup horizontal>
+                  <Card style={{ width: '30rem' }}>
+                    <Card.Header>Market Range</Card.Header>
+                    <Card.Body>{stockInfo.price.regularMarketDayLow.fmt} - {stockInfo.price.regularMarketDayHigh.fmt}</Card.Body>
+                  </Card>
+                </ListGroup>
+                <ListGroup horizontal>
+                  <Card style={{ width: '15rem' }}>
+                    <Card.Header>Sector</Card.Header>
+                    <Card.Body>{stockInfo.summaryProfile.sector}</Card.Body>
+                  </Card>
+                  <Card style={{ width: '15rem' }}>
+                    <Card.Header>Market Cap</Card.Header>
+                    <Card.Body>{stockInfo.price.marketCap.longFmt}</Card.Body>
+                  </Card>
+                </ListGroup>
               </ListGroup>
             </Card.Body>
           </Card>
